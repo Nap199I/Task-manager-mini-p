50f2b610348cb770566000369659b1b848e95845
Nap199I <nathan_parry15@hotmail.com>
2023-07-23 10:28:04 +0000

    Add add tasks function

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,9 +2,19 @@ document.addEventListener('DOMContentLoaded', function() {
     // sidenav initialization
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
-  });
 
-document.addEventListener('DOMContentLoaded', function() {
+    // category model
     let modal = document.querySelectorAll('.modal');
     M.Modal.init(modal);
+
+    // datepicker initialization
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+      format: "dd mmm, yyyy",
+      i18n: {done:"select"}
+    });
+
+    // select initialization
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
   });
